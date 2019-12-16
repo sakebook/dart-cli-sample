03e57cbfabc2e1a5cd2b623cbcf5317397114eb4
sakebook <sakebook@gmail.com>
2019-12-16 20:57:16 +0900

    Modify output

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:dart_cli_sample/dart_cli_sample.dart' as dart_cli_sample;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_cli_sample.calculate()}!');
+  stdout.writeln('Hello ${dart_cli_sample.system()}!');
+  exitCode = 0;
 }
--- lib/dart_cli_sample.dart
+++ lib/dart_cli_sample.dart
@@ -1,3 +1,5 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+String system() {
+  return Platform.operatingSystem;
 }

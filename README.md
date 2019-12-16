a9f81db700662c682da5e6e49dae485ae46eabd8
sakebook <sakebook@gmail.com>
2019-12-16 21:12:38 +0900

    Fix test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# dart-cli-sample
+
 A sample command-line application.
 
 Created from templates made available by Stagehand under a BSD-style
--- test/dart_cli_sample_test.dart
+++ test/dart_cli_sample_test.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
+
 import 'package:dart_cli_sample/dart_cli_sample.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('system', () {
+    if (Platform.isLinux) {
+      expect(system(), 'linux');
+    } else if (Platform.isWindows) {
+      expect(system(), 'windows');
+    } else if (Platform.isMacOS) {
+      expect(system(), 'macos');
+    } else {
+      fail('Not supported OS');
+    }
   });
 }
